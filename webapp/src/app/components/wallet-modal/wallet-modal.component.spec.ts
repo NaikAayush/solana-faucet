c2bd5633c02630b52eabdcfb9a167de9146b0d65
NaikAayush <ayush@pesu.pes.edu>
2022-01-15 00:01:01 +0530

    feat: modal component

---
+++ webapp/src/app/components/wallet-modal/wallet-modal.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WalletModalComponent } from './wallet-modal.component';
+
+describe('WalletModalComponent', () => {
+  let component: WalletModalComponent;
+  let fixture: ComponentFixture<WalletModalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WalletModalComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WalletModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ webapp/src/app/components/wallet-modal/wallet-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wallet-modal',
+  templateUrl: './wallet-modal.component.html',
+  styleUrls: ['./wallet-modal.component.css']
+})
+export class WalletModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
